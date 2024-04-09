cb4124c60d446fa1064cc4be8cf5b0999f264cf9
ochogric <158656986+ochogric@users.noreply.github.com>
2024-04-08 21:01:45 -0700

    Create cis129_lab10.py

---
+++ cis129_lab10.py
@@ -0,0 +1,8 @@
+# Input the dollar amount
+dollar_amount = input("Enter the dollar amount: $")
+
+# Format the dollar amount in check-protected format
+formatted_amount = f"**{dollar_amount:9}"
+
+# Print the formatted amount
+print(formatted_amount)
